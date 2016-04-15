8350c88c4ee5eb34387d3b05beb7d8edd75cf500
anton <akropp@gmail.com>
2016-04-15 12:52:23 -0700

    readme

--- src/main/java/io/paradoxical/SchemaCompareOptions.java
+++ src/main/java/io/paradoxical/SchemaCompareOptions.java
@@ -4,9 +4,18 @@ import lombok.Data;
 
 @Data
 public class SchemaCompareOptions {
+    /**
+     * Fail if array lengths between master and compare dont match (i.e. element mismatches)
+     */
     private Boolean failOnMisMatchedArrayLengths = false;
 
+    /**
+     * Fail if the compared item does not have a field the master does
+     */
     private Boolean failOnMissingObject = false;
 
+    /**
+     * Fail if the master is missing fields the compare does
+     */
     private Boolean failIfMasterIsMissingProperties = false;
 }
